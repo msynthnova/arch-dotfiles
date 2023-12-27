49b8ff8231d1b174532ce1f5ca64b6bded76a08f
Felix <kcong.nguyen@icloud.com>
2023-12-27 00:42:50 -0600

    *

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard xdg-desktop-portal-hyprland
-sudo pacman -S firefox hyprland wayland kitty zsh gcc hyprpaper waybar
+sudo pacman -S firefox hyprland wayland kitty zsh gcc hyprpaper waybar bluez bluez-utils pipewire pipewire-audio pipewire-alsa alsa-utils wireplumber
 sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
