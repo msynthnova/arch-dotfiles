1df900ab1928f860a4a801e16bbb3ba95cdc1ca7
Felix <kcong.nguyen@icloud.com>
2023-12-26 23:57:17 -0600

    *

--- packages.sh
+++ packages.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S ripgrep htop tmux neofetch btop
-sudo pacman -S firefox hyprland wayland kitty zsh gcc
+sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard xdg-desktop-portal-hyprland
+sudo pacman -S firefox hyprland wayland kitty zsh gcc hyprpaper waybar
+sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
--- setup.sh
+++ setup.sh
@@ -4,6 +4,10 @@ cp .tmux.conf ~/
 cp .zshrc ~/
 cp kitty.conf ~/.config/kitty/
 cp hyprland.conf ~/.config/hypr
+cp hyprpaper.conf ~/.config/hypr
+cp -r media ~/.config/hypr
+cp config ~/.config/waybar
+cp style.css ~/.config/waybar
 
 git config --global credential.helper store
 git config --global user.name "Felix"
