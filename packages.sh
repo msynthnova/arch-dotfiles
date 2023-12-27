3b382618d85b1b080c765da50372af8ea049d8e2
Felix <kcong.nguyen@icloud.com>
2023-12-27 00:58:15 -0600

    *

--- packages.sh
+++ packages.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard xdg-desktop-portal-hyprland
-sudo pacman -S firefox hyprland wayland kitty zsh gcc hyprpaper waybar bluez bluez-utils pipewire pipewire-audio pipewire-alsa alsa-utils wireplumber
+sudo pacman -S firefox hyprland wayland kitty zsh gcc hyprpaper waybar bluez bluez-utils 
+# sudo pacman -S pipewire pipewire-audio pipewire-alsa alsa-utils wireplumber
+sudo pacman -S pulseaudio pulseaudio-alsa pamixer alsa-utils pulsemixer
 sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
