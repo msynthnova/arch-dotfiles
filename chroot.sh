dd92efa9ed0773e43a2033b07794d9e11884ee3d
Felix <kcong.nguyen@icloud.com>
2023-12-26 19:50:04 -0600

    *

---
+++ chroot.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+echo "felixhost" > /etc/hostname
+echo "127.0.0.1    localhost" > /etc/hosts
+echo "::1          localhost" > /etc/hosts
+echo "127.0.1.1    felixhost" >> /etc/hosts
+passwd
+useradd -m -G wheel felix
+passwd felix
+echo "root ALL=(ALL:ALL) ALL" > /etc/sudoers
+echo "%wheel ALL=(ALL:ALL) ALL" >> /etc/sudoers
+echo "@includedir /etc/sudoers.d" >> /etc/sudoers
+
+grub-install --target=x86_64-efi --bootloader-id=grub
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
---
+++ parted.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# partition
+wipefs -a /dev/nvme0n1
+parted --script /dev/nvme0n1 mklabel gpt
+parted --script /dev/nvme0n1 mkpart ESP fat32 1MiB 51MiB
+parted --script /dev/nvme0n1 name 1 "EFI_System_Partition"
+parted --script /dev/nvme0n1 set 1 boot on
+parted --script /dev/nvme0n1 mkpart primary linux-swap 51MiB 10101MiB
+parted --script /dev/nvme0n1 name 2 "Linux_Swap_Partition"
+parted --script /dev/nvme0n1 mkpart primary ext4 10101MiB 100%
+parted --script /dev/nvme0n1 name 3 "Linux_FileSystem_Partition"
+
+# fs type
+mkfs.fat -F 32 /dev/nvme0n1p1
+mkswap /dev/nvme0n1p2
+mkfs.ext4 /dev/nvme0n1p3
+
+# mount
+mount --mkdir /dev/nvme0n1p1 /mnt/boot/efi
+swapon /dev/nvme0n1p2
+mount /dev/nvme0n1p3 /mnt
+
+# init
+pacstrap -K /mnt base linux linux-firmware sudo networkmanager git neovim intel-ucode grub efibootmgr
+genfstab -U /mnt >> /mnt/etc/fstab
