de0d1c30d4beeb195980874b97b6b6114a534cc1
Felix <kcong.nguyen@icloud.com>
2023-12-29 19:14:11 -0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard alacritty
+sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard kitty
 sudo pacman -S firefox hyprland wayland zsh gcc hyprpaper waybar bluez bluez-utils
 sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
