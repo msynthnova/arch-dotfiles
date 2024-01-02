424b9040600d207bd3286d4629381eeb5abbf6f2
Felix <kcong.nguyen@icloud.com>
2024-01-01 19:23:36 -0600

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -3,4 +3,4 @@
 sudo pacman -S ripgrep htop tmux neofetch btop wl-clipboard kitty
 sudo pacman -S firefox hyprland wayland zsh gcc hyprpaper waybar
 sudo pacman -S alsa-utils pipewire wireplumber
-sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts
+sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji
