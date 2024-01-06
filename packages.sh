6d39bfe758b5cc2bc1ec89b24efdad0064d8cd9e
Felix <kcong.nguyen@icloud.com>
2024-01-06 01:19:33 -0600

    *

--- packages.sh
+++ packages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo pacman -S ripgrep btop tmux neofetch wl-clipboard kitty mandb
-sudo pacman -S firefox wayland zsh gcc sway swaybg
+sudo pacman -S firefox wayland zsh gcc sway swaybg clang
 sudo pacman -S alsa-utils pipewire wireplumber
 sudo pacman -S ttf-fira-code adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts noto-fonts-cjk noto-fonts-emoji ttf-dejavu gnu-free-fonts
