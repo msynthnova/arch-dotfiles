6b72f4ce9d5b56229050a721bd5ce031035246b5
Felix <kcong.nguyen@icloud.com>
2024-01-06 20:31:57 -0600

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -27,5 +27,4 @@ grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=ARCH_
 grub-mkconfig -o /boot/grub/grub.cfg
 
 systemctl enable NetworkManager
-systemctl enable iwd
 systemctl enable bluetooth
