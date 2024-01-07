0e23b7cc7c318227392b6de420924f7f3edced83
Felix <kcong.nguyen@icloud.com>
2024-01-06 21:41:06 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 cp .tmux.conf .zshrc ~/
 cp kitty.conf ~/.config/kitty/
 cp media/wallhaven.jpg config ~/.config/sway/
+cp -r fonts ~/.local/share
 
 git config --global credential.helper store
 git config --global user.name "Felix"
